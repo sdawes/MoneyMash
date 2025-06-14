f7a513c13078efbc2df991223855195e014864fd
Stephen Dawes <stephenrdawes@gmail.com>
2025-06-14 12:03:50 +0100

    Initial Commit

---
+++ MoneyMash/MoneyMashApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyMashApp.swift
+//  MoneyMash
+//
+//  Created by Stephen Dawes on 14/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoneyMashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
